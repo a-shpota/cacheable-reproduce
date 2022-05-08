8acccb2eb351981d03b29029de850b3705206da0
Shpota Andrew <shpota@rozetka.ua>
2022-05-08 15:12:34 +0300

    add universal & use CacheableLookup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cacheable-reproduce';
+  constructor(httpClient: HttpClient, ngZone: NgZone) {
+
+    ngZone.runOutsideAngular(() => {
+      setInterval(() => {
+        var taskTrackingZone = (<any>ngZone)._inner.getZoneWith("TaskTrackingZone");
+        if (!taskTrackingZone) {
+          throw new Error("'TaskTrackingZone' zone not found! Have you loaded 'node_modules/zone.js/dist/task-tracking.js'?");
+        }
+        var tasks: any[] = taskTrackingZone._properties.TaskTrackingZone.getTasksFor("macroTask") || [];
+        console.log("ZONE pending tasks=", tasks.map(task => task.creationLocation));
+      }, 1000);
+    })
+
+    httpClient.get('https://www.google.com', {
+      responseType: 'text'
+    }).subscribe(() => {
+      console.log('Request from Google complete');
+    });
+  }
 }
